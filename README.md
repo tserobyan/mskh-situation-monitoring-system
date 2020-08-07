f4dca906bde366e9da7cf372a8e053a65fb372a5
Sargis Markosyan <sargismarkosyan@workfront.com>
2020-08-07 15:31:45 +0400

    add the doc link, add check-status route

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Make sure that the app is running at http://localhost:3000
 ### About the system
 - Framework: https://expressjs.com/
 - Getting started: https://expressjs.com/en/starter/installing.html
+
+## The project description
+https://docs.google.com/document/d/17BEhmiX_KxHTWM5y6Io7krdNymJU94n6bxXNdQ255tg/edit?usp=sharing
--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/check-status', (req, res) => {
+    //@TODO send request to the mskh.am
+    const responceTime = 9070 //in ms (1s = 1000ms)
+    res.send(`${responceTime}`)
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
