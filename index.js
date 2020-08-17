04838a6f616d78237f374eeafb52f63ac6fd62ab
tigran-serobyan <tigran1serobyan@gmail.com>
2020-08-17 18:39:57 +0400

    Sends request to mskh.am

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const request = require('request')
-const { MongoClient } = require('mongodb')
 const app = express()
 const port = 3000
 
@@ -20,29 +19,3 @@ app.get('/check-status', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-async function main() {
-  const uri = "mongodb://localhost:27017/";
-
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-
-    await listDatabases(client);
-
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-
-main().catch(console.error);
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-}
