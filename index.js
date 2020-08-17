18ce2b201261231e76ef124bb8c8be8aec3c17a0
tigran-serobyan <tigran1serobyan@gmail.com>
2020-08-17 17:16:22 +0400

    Connected to Database

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const express = require('express')
 const request = require('request')
+const { MongoClient } = require('mongodb')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/check-status', (req, res) => {
-    request.get({
-        url : 'https://mskh.am/',
-        time : true
-      },function(err, response){
-        if(!err){
-          res.send(`${response.elapsedTime}`)
-        } else{
-          res.send(err.code)
-        }
-      })
+  request.get({
+    url: 'https://mskh.am/',
+    time: true
+  }, function (err, response) {
+    if (!err) {
+      res.send(`${response.elapsedTime}`)
+    } else {
+      res.send(err.code)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+async function main() {
+  const uri = "mongodb://localhost:27017/";
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
