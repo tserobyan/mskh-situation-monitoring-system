076ee7a3ee9213bed3876c375ef1c16fede34d94
tatev <tatevayvazyan@mskh.am>
2020-08-18 16:03:52 +0400

    Connected to mongoDB with mongoose

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const User = require('./models/user')
+mongoose.connect('mongodb://localhost:27017/myDb', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection
+    .on('error', console.log)
+    .once('open', () => {
+        console.log('Successfully connected to MongoDB');
+    });
+
+    const tatev = new User({
+        name: 'Tatev'
+    })
+
+    tatev.save()
+    .then(doc => {
+        console.log(doc)
+    })
+    .catch(err => {
+        console.error(err)
+    });
+
+    User
+        .find()
+        .then((err, users) => {
+            if (err) {
+                 console.log(err);
+            } else {
+                console.log(users);
+            }
+            
+        });
+      
 const app = express()
 const port = 3000
 
