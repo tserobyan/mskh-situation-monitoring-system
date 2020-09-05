9078caafe1a58c17dc72ddf31f0ab5afd3c5c57b
tigran-serobyan <tigran1serobyan@gmail.com>
2020-09-05 17:17:58 +0400

    Chart fixed

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,6 @@ var express = require('express');
 var request = require('request');
 const { addRequest, getRequests } = require('../services/request');
 var router = express.Router();
-var data = [
-  { date: '2020-08-27 6:00', timing: 2082 },
-  { date: '2020-08-27 9:00', timing: 1039 },
-  { date: '2020-08-27 12:00', timing: 1049 },
-  { date: '2020-08-27 15:00', timing: 2574, error: 'ERRORCODE' },
-  { date: '2020-08-27 18:00', timing: 1716 },
-  { date: '2020-08-27 21:00', timing: 1130 },
-  { date: '2020-08-28 00:00', timing: 1477 },
-  { date: '2020-08-28 3:00', timing: 3875, error: 'ERRORCODE' },
-  { date: '2020-08-28 6:00', timing: 1375 },
-  { date: '2020-08-28 9:00', timing: 1078 },
-  { date: '2020-08-28 12:00', timing: 1093 },
-  { date: '2020-08-28 15:00', timing: 2355 },
-];
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
