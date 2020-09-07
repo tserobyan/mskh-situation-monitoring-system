d6520a9668f9c72bb418bd6b40ee36d56b883cc0
tigran-serobyan <tigran1serobyan@gmail.com>
2020-09-07 19:02:01 +0400

    Chart fixed

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,16 +1,24 @@
 var chartData = [];
 var maxdate = null;
+var maxvalue = null;
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable(chartData);
     var options = {
         height: 600,
-        vAxis: { minValue: 0 },
-        hAxis:{
-            viewWindow:{
-                min:new Date(maxdate-50000000),
-                max:new Date(maxdate+2000000),
-            }
+        vAxis: {
+            minValue: 0,
+            viewWindow: {
+                min: 0,
+                max: Math.ceil(maxvalue / 1000) * 1000,
+            },
+        },
+        hAxis: {
+            viewWindow: {
+                min: new Date(maxdate - 41000000),
+                max: new Date(maxdate + 1500000),
+            },
+            format: 'HH:mm'
         },
         explorer: {
             actions: ['dragToPan', 'rightClickToReset'],
@@ -19,7 +27,7 @@ function drawChart() {
         focusTarget: 'category',
         tooltip: { isHtml: true }
     };
-    
+
     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
     chart.draw(data, options);
 }
@@ -32,8 +40,11 @@ function main(data) {
     ]);
     for (let i of data) {
         let date = new Date(i.date);
-        if(date.getTime() > maxdate){
+        if (date.getTime() > maxdate) {
             maxdate = date.getTime();
+        }
+        if (i.duration > maxvalue) {
+            maxvalue = i.duration;
         }
         chartData.push([
             date,
